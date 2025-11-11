590891fa84d669215fb76478a86b30742f1c3ba2
qaz00217 <113839959+qaz00217@users.noreply.github.com>
2025-11-11 15:40:16 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+
+player_score = 0
+
+def update_score(amount):
+    global player_score
+    player_score += amount
+    print(f'Score changed: {player_score}')
+
+def main():
+    update_score(5)
+sd
+//블럭
