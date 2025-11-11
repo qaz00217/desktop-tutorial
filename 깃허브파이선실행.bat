f9b9970f03ecc89cf070922eb48e2930900eb052
qaz00217 <113839959+qaz00217@users.noreply.github.com>
2025-11-12 00:33:55 +0900

    Add files via upload

---
+++ 깃허브파이선실행.bat
@@ -0,0 +1,15 @@
+@echo off
+REM ==============================
+REM GitHub 환경변수 설정
+REM ==============================
+set GITHUB_USER=qaz00217
+set GITHUB_TOKEN=123
+
+REM ==============================
+REM 새로운 cmd에서 Python 실행
+REM ==============================
+REM 현재 폴더에 github_dnd_uploader.py가 있어야 함
+python github_dnd_uploader.py
+
+
+exit
